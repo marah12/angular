e8f6697c7c22b837f7798bb27a0725e40cbd1292
marah12 <201706351@bethlehem.edu>
2020-07-08 15:50:53 +0300

    App folder

---
+++ app-routing.module.ts
@@ -0,0 +1,76 @@
+// Angular
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+// Components
+import { BaseComponent } from './views/theme/base/base.component';
+// Auth
+import { AuthGuard } from './core/auth';
+import {HomeComponent} from './views/pages/employees/home/home.component';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { UsersListComponent } from './views/pages/user-management/users/users-list/users-list.component';
+
+const routes: Routes = [
+  {path: 'auth', loadChildren: () => import('./views/pages/auth/auth.module').then(m => m.AuthModule)},
+  {path: 'error', loadChildren: () => import('./views/pages/error/error.module').then(m => m.ErrorModule)},
+  {
+    path: '',
+    component: BaseComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./views/pages/dashboard/dashboard.module').then(m => m.DashboardModule),
+      },
+      {
+        path: 'mail',
+        loadChildren: () => import('./views/pages/apps/mail/mail.module').then(m => m.MailModule),
+      },
+      {
+        path: 'ecommerce',
+        loadChildren: () => import('./views/pages/apps/e-commerce/e-commerce.module').then(m => m.ECommerceModule),
+      },
+      {
+        path: 'ngbootstrap',
+        loadChildren: () => import('./views/pages/ngbootstrap/ngbootstrap.module').then(m => m.NgbootstrapModule),
+      },
+      {
+        path: 'material',
+        loadChildren: () => import('./views/pages/material/material.module').then(m => m.MaterialModule),
+      },
+      {
+        path: 'user-management',
+        loadChildren: () => import('./views/pages/user-management/user-management.module').then(m => m.UserManagementModule),
+      },
+      {
+        path: 'wizard',
+        loadChildren: () => import('./views/pages/wizard/wizard.module').then(m => m.WizardModule),
+      },
+      {
+        path: 'builder',
+        loadChildren: () => import('./views/theme/content/builder/builder.module').then(m => m.BuilderModule),
+      },
+      { 
+        path:'home',
+component: HomeComponent
+      },
+      {
+      path:'users',
+      component:UsersListComponent
+            },
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+      {path: '**', redirectTo: 'dashboard', pathMatch: 'full'},
+    ],
+  },
+  {path: '**', redirectTo: 'error/403', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
